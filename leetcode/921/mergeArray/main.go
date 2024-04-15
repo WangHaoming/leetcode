d4d6193d88327aba65d3291fe2b0b75355c71b4d
haoming.wang <wanghaoming100@gmail.com>
2024-04-15 22:15:11 +0900

    sort

---
+++ leetcode/921/mergeArray/main.go
@@ -0,0 +1,15 @@
+package mergeArray
+
+import "fmt"
+
+// https://leetcode.com/problems/sort-an-array/solutions/472053/Go-golang-Quick-Sort-solutions/
+
+func sortArray(nums []int) []int {
+	return nums
+}
+
+func Test() {
+	nums := []int{2, 6, 1, 7, 9, 11, 21, 3, 5, 8, 22, 55, 3}
+	sortArray(nums)
+	fmt.Println(nums)
+}
